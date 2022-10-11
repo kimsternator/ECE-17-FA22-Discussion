d8a1340350941d30851a44ced9f7b9c5649fbb9b
kimsternator <sskim@ucsd.edu>
2022-10-11 15:25:31 -0700

    post discussion

--- Discussion 3/main.cpp
+++ Discussion 3/main.cpp
@@ -15,28 +15,58 @@ void printMap(map<int, size_t> aMap);
 void printMap(unordered_map<int, size_t> aMap);
 
 bool isPunct(char aChar) {
-	static std::string punct(".;?!:,'-");
+	static std::string punct(".;?!:,'-\nA");
 	return std::string::npos != punct.find(aChar);
 }
 
 void runFrequencyExample() {
-	vector<int> numbers = {1, 2, 3, 4, 5, 2, 3, 2, 4, 5};
-	map<int, size_t> theFrequencies;
-//	unordered_map<int, size_t> theFrequencies;
-
+	vector<int> numbers = {3, 1, 4, 5, 3, 4, 5};
+//	map<int, size_t> theFrequencies;
+	unordered_map<int, size_t> theFrequencies;
+    for (size_t i = 0; i < numbers.size(); i++) {
+        if (theFrequencies.find(numbers[i]) == theFrequencies.end()) { // key not found
+            theFrequencies[numbers[i]] = 1;
+        } else {
+            theFrequencies[numbers[i]] += 1;
+        }
+        printMap(theFrequencies);
+        std::cout << endl;
+    }
 }
 
 void runFileExample() {
 	/*
-	 * g, p pointers
+	 * g, p pointers (get, put)
+	 *  tell, seek
 	 * get, getline, >> operator
 	 */
-	string filename = R"(S:\Documents\TA\ECE 17\FA22\Discussion 3\text.txt)";
+	string filename = R"(C:\Users\steph\Documents\Class\TA\ECE 17\FA22\ECE-17-FA22-Discussion\Discussion 3\text.txt)";
 	fstream stream(filename);
+
+	if (!stream.is_open()) {
+	    return;
+	}
+//    cout << (char) stream.get() << endl;
+//    cout << (char) stream.get() << endl;
+//    cout << (char) stream.get() << endl;
+//    cout << (std::string) stream.getline()
+//    stream.seekg(0);
+//	cout << stream.tellg() << " " << stream.tellp();
+	string temp;
+	while (stream >> temp) {
+	    cout << temp << ((stream.eof() || isPunct((char) stream.peek())) ? "" : "_");
+	    while (stream.peek() == '\n') {
+	        cout << "\n";
+	        stream.get();
+	    }
+	}
 }
 
 void runUserInputExample() {
-
+    cout << "Enter an input: ";
+    string userInput;
+    std::cin >> userInput;
+    cout << userInput;
 }
 
 void runStringCleaningExample() {
@@ -48,7 +78,6 @@ void runStringCleaningExample() {
 	cout << a << endl;
 	a.erase(std::remove_if(a.begin(), a.end(), isPunct), a.end());
 	cout << a << endl;
-
 }
 
 void runInheritanceExample() {
