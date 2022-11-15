5fa39799b85166b943efb6dd2497ac55150b41a6
kimsternator <sskim@ucsd.edu>
2022-11-14 17:00:18 -0800

    Post Discussion Commit

--- Discussion 8/main.cpp
+++ Discussion 8/main.cpp
@@ -11,6 +11,8 @@ using std::cout, std::endl,
 		std::pair;
 
 using IntPair = pair<int, int>;
+using WordFreq = map<string, size_t>;
+using FreqPair = pair<string, size_t>;
 
 template <typename T>
 void printVector(vector<T>& aVec, string aSep="", string anEnd="\n") {
@@ -38,7 +40,11 @@ static map<string, function<void(ifstream& theInput)>> argumentFunctions = {
 
 void outputFile(vector<string> aWords) {
 	for (auto &word: aWords) {
-		cout << word;
+	    if (word == "paragraph") {
+	        cout << "_";
+	    } else {
+            cout << word;
+        }
 		if (word.back() != '\n') {
 			cout << " ";
 		}
@@ -57,17 +63,56 @@ void vectorExample(ifstream& theInput) {
 	}
 
 	outputFile(words);
+	// Get Stream flags
+//    cout << std::hex << theInput.flags() << endl;
+}
+
+bool customComparator(const std::pair<int, int>& aLeft, const std::pair<int, int>& aRight) {
+    if (aLeft.second != aRight.second) {
+        return aLeft.second < aRight.second;
+    }
+    return aLeft.first < aRight.first;
 }
 
 void streamExample(ifstream& theInput) {
 	vector<std::pair<int, int>> vec = {
 			{1, 2},
-			{3, 4},
+			{3, 0},
 			{1, 3},
-			{1, 4},
+			{1, 2},
+			{2, 1},
 			{2, 3},
-			{2, 4},
 	};
+    int aNum = 3;
+    printVector(vec, ", ");
+    std::sort(vec.begin(), vec.end(), [&vec, aNum](const std::pair<int, int>& aLeft, const std::pair<int, int>& aRight){
+        if (aLeft.second != aRight.second) {
+            return aLeft.second < aRight.second;
+        }
+        return aLeft.first < aRight.first;
+    });
+    printVector(vec, ", ");
+
+    // Sorting a vector of ints
+//	vector<int> anotherVec = {2, 3, 6, 2, 1, 2};
+//    printVector(anotherVec, ", ");
+//    std::sort(anotherVec.begin(), anotherVec.end(), customComparator);
+//    printVector(anotherVec, ", ");
+
+    // Resetting the flags example
+//    WordFreq words;
+//    string currentWord;
+//    while (theInput >> currentWord) {
+//        while (!theInput.eof() && theInput.peek() == '\n') {
+//            currentWord += "\n";
+//            theInput.get();
+//        }
+//        words[currentWord]++;
+//    }
+//    theInput.clear();
+//    theInput.seekg(0, std::ios::beg);
+//    theInput >> currentWord;
+//    int a = 1;
 }
 
 int main(int argc, char* argv[]) {
@@ -80,5 +125,6 @@ int main(int argc, char* argv[]) {
 		return -1;
 	}
 	argumentFunctions[argv[1]](theFile);
+
 	return 0;
 }
